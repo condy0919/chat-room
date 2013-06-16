6e12da7c625bce1c1095abd15e402fc3d8a22567
condy0919 <czw19931006@hotmail.com>
2013-06-16 22:22:04 +0800

    the first window

---
+++ gtkMain.c
@@ -0,0 +1,37 @@
+#include <gtk/gtk.h>
+
+void check_button_callback(GtkWidget* widget, gpointer data)
+{
+	if (gtk_check_button_get_active(GTK_CHECK_BUTTON(widget))) {
+		g_print("oh, you pressed check button.\n");
+	} else {
+		; // do nothing
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	GtkWidget* window;
+	GtkWidget* check_button;
+
+	gtk_init(&argc, &argv);
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	g_signal_connect(G_OBJECT(window), "destroy",
+			G_CALLBACK(gtk_main_quit), window);
+	g_signal_connect(G_OBJECT(window), "delete_event",
+			G_CALLBACK(gtk_main_quit), window);
+	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+
+	check_button =  gtk_check_button_new_with_label("first check button");
+	gtk_container_add(GTK_CONTAINER(window), check_button);
+	g_signal_connect(G_OBJECT(check_button), "checked",
+			G_CALLBACK(check_button_callback), NULL);
+
+	gtk_widget_show(check_button);
+	gtk_widget_show(window);
+
+	gtk_main();
+
+	return 0;
+}
