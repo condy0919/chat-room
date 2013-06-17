150fc1e485c1c4d6055db9caf1785c01d55d1d0e
condy0919 <condy0919@gmail.com>
2013-06-17 21:59:00 +0800

    仅仅修改一点点，因为知道那个操作不是必须的。所以...

--- gtkMain.c
+++ gtkMain.c
@@ -2,7 +2,7 @@
 
 void check_button_callback(GtkWidget* widget, gpointer data)
 {
-	if (gtk_check_button_get_active(GTK_CHECK_BUTTON(widget))) {
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget))) {
 		g_print("oh, you pressed check button.\n");
 	} else {
 		; // do nothing
@@ -18,14 +18,14 @@ int main(int argc, char* argv[])
 
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	g_signal_connect(G_OBJECT(window), "destroy",
-			G_CALLBACK(gtk_main_quit), window);
+			G_CALLBACK(gtk_main_quit), NULL);
 	g_signal_connect(G_OBJECT(window), "delete_event",
 			G_CALLBACK(gtk_main_quit), window);
 	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 
 	check_button =  gtk_check_button_new_with_label("first check button");
 	gtk_container_add(GTK_CONTAINER(window), check_button);
-	g_signal_connect(G_OBJECT(check_button), "checked",
+	g_signal_connect(G_OBJECT(check_button), "toggled",
 			G_CALLBACK(check_button_callback), NULL);
 
 	gtk_widget_show(check_button);
