e0964309e238e6dc7065ee207582c010daec8d2a
condy0919 <condy0919@gmail.com>
2013-06-18 10:19:15 +0800

    把login界面做完了。

--- gtkMain.c
+++ gtkMain.c
@@ -1,35 +1,18 @@
 #include <gtk/gtk.h>
+#include <glib.h>
+#include "gui.h"
 
-void check_button_callback(GtkWidget* widget, gpointer data)
-{
-	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget))) {
-		g_print("oh, you pressed check button.\n");
-	} else {
-		; // do nothing
-	}
-}
+GString* user_name;
+GtkWidget* main_app_window;
 
 int main(int argc, char* argv[])
 {
-	GtkWidget* window;
-	GtkWidget* check_button;
+	GtkWidget* login_window;
 
 	gtk_init(&argc, &argv);
 
-	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	g_signal_connect(G_OBJECT(window), "destroy",
-			G_CALLBACK(gtk_main_quit), NULL);
-	g_signal_connect(G_OBJECT(window), "delete_event",
-			G_CALLBACK(gtk_main_quit), window);
-	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+	login_window = create_login_window();
 
-	check_button =  gtk_check_button_new_with_label("first check button");
-	gtk_container_add(GTK_CONTAINER(window), check_button);
-	g_signal_connect(G_OBJECT(check_button), "toggled",
-			G_CALLBACK(check_button_callback), NULL);
-
-	gtk_widget_show(check_button);
-	gtk_widget_show(window);
 
 	gtk_main();
 
